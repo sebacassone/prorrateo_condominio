e0dfad45899fac5c1181ffd873f766d7ca11e911
Sebastian <sebastian.gonzalez.cass@usach.cl>
2023-11-30 16:23:55 -0300

    feat: connection database is finished

--- api/src/main/java/cl/soge/api/models/contratoModel.java
+++ api/src/main/java/cl/soge/api/models/contratoModel.java
@@ -1,12 +1,18 @@
 package cl.soge.api.models;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Table(name = "Contrato")
 public class contratoModel {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id_contrato", nullable = false)
+    @Column(name = "id_contrato", nullable = false, unique = true)
     private Integer idContrato;
     @Column(name = "calificar_contrato")
     private Boolean calificarContrato;
