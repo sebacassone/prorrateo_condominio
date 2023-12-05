224ee47f6066884a7c916f2f23cae96a8d5c92be
Bastián  Brito Garrido <bastian.brito.g@usach.cl>
2023-12-05 15:13:33 -0300

    se sube correctamente vista gasto comun

--- landing/frontend/src/main.js
+++ landing/frontend/src/main.js
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import '@mdi/font/css/materialdesignicons.css' // Ensure you are using css-loader
 
 // Vuetify
 import 'vuetify/styles'
