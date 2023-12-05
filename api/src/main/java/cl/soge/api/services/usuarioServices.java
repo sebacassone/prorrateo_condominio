6eaee6a07d8854ca28ddebe1974fd56647de7b7d
Sebastian <sebastian.gonzalez.cass@usach.cl>
2023-12-05 16:43:08 -0300

    fix: login for users without propieties

--- api/src/main/java/cl/soge/api/services/usuarioServices.java
+++ api/src/main/java/cl/soge/api/services/usuarioServices.java
@@ -35,13 +35,27 @@ public class usuarioServices {
 
             // Asumiendo que el nombre y tipo de usuario son iguales para todas las filas
             respuesta.put("nombre", resultados.get(0)[2]); // Cambiar el índice si es necesario
-            respuesta.put("tipoUsuario", resultados.get(0)[3]); // Cambiar el índice si es necesario
+            respuesta.put("apellido", resultados.get(0)[3]); // Cambiar el índice si es necesario
+            respuesta.put("tipoUsuario", resultados.get(0)[4]); // Cambiar el índice si es necesario
             respuesta.put("idEdificio", idEdificio);
             respuesta.put("idPropiedades", idsPropiedades);
 
             return respuesta;
         } else {
-            return null;
+            resultados = usuarioRepository.buscarUsuario(rut, password);
+            if (resultados != null && !resultados.isEmpty()) {
+                Map<String, Object> respuesta = new HashMap<>();
+
+                respuesta.put("nombre", resultados.get(0)[0]); // Cambiar el índice si es necesario
+                respuesta.put("apellido", resultados.get(0)[1]); // Cambiar el índice si es necesario
+                respuesta.put("tipoUsuario", resultados.get(0)[2]); // Cambiar el índice si es necesario
+                respuesta.put("idEdificio", null);
+                respuesta.put("idPropiedades", null);
+
+                return respuesta;
+            } else {
+                return null;
+            }
         }
     }
 }
