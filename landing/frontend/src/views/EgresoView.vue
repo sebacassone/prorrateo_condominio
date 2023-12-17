42dbb2e0e0c53f69444ee66b9f0678bbdafe38a7
unknown <byron.caices@usach.cl>
2023-12-17 00:43:57 -0300

    egreso fixed2

--- landing/frontend/src/views/EgresoView.vue
+++ landing/frontend/src/views/EgresoView.vue
@@ -249,7 +249,7 @@
 
         methods: {
             initFetch() {
-            /*
+            
             // Obtener el objeto de usuario de localStorage
                 const storedUserDataJSON = localStorage.getItem('userData');
                 if (storedUserDataJSON) {
@@ -275,7 +275,7 @@
                   // userData no existe en localStorage, redirigir a login
                   this.$router.push('/login');
                 }
-                */
+                
             },
             async sendEgresoForm() {
                 try {
